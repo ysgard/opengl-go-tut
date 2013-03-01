6eb97b5680b4d9f7abb0da26001c5ebcab8a0558
ysgard <ysgard@gmail.com>
2013-02-28 22:19:23 -0800

    Finishd basic text file reader

--- test_source_reader.go
+++ test_source_reader.go
@@ -7,24 +7,42 @@ package main
 import (
 	"fmt"
 	"os"
+	"bytes"
 	"bufio"
+	"io"
 )
 
+// Reads a file and returns its contents as a string.
 func ReadSourceFile(filename string) (string, error) {
-	if fp, err := os.Open(filename); err != nil {
-		fmt.Fprintf(os.Stderr, "Could not open %s!\n", filename)
+	
+	fp, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ReadSourceFile: Could not open %s!\n", filename)
 		fmt.Fprintf(os.Stderr, "os.Open: %e\n", err)
 		return "", err
 	}
-	var lines []string
-	for line, err := 
+	defer fp.Close()
+
+	r := bufio.NewReaderSize(fp, 4*1024)
+	var buffer bytes.Buffer
+	for {
+		line, err := r.ReadString('\n')
+		buffer.WriteString(line)
+		if err == io.EOF {
+			// We've read the last string. Make sure there's an end of line.
+			buffer.WriteByte('\n')
+			break
+		}
+	}
+	return buffer.String(), nil
 
 }
 
 func main() {
-	if codeString, err := ReadSourceFile("simple_vertex_shader.glsl"); err != nil {
+	
+	if codeString, err := ReadSourceFile("simple_fragment_shader.glsl"); err == nil {
+		fmt.Fprintf(os.Stdout, codeString)
+	} else {
 		fmt.Fprintf(os.Stderr, "Could not read source code! : %s\n", err)
-		return
 	}
-	fmt.Fprintf(os.Stdout, codeString)
 }
