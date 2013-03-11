5fdf9a251e6f629dcc2fe01c4d47659ec4928d65
ysgard <ysgard@gmail.com>
2013-03-11 15:41:35 -0700

    Merged tutorial05

--- tutorial05.go
+++ tutorial05.go
@@ -8,8 +8,8 @@ package main
 import (
 	"fmt"
 	gl "github.com/chsc/gogl/gl33"
-	"github.com/jragonmiris/mathgl"
 	"github.com/go-gl/glfw"
+	"github.com/jragonmiris/mathgl"
 	"os"
 	"runtime"
 	"unsafe"
@@ -42,7 +42,6 @@ func loadTGA(imagePath string) gl.Uint {
 	// Return the ID of the texture we just created
 	return txid
 }
-
 
 func main() {
 	runtime.LockOSThread()
@@ -111,10 +110,8 @@ func main() {
 	// Model matrix: and identity matrix (model will be at the origin)
 	model := mathgl.Ident4f() // Changes for each model!
 
-
 	// Our ModelViewProjection : multiplication of our 3 matrices - remember, matrix mult is other way around
 	MVP := projection.Mul4(view).Mul4(model) // projection * view * model
-
 
 	// An array of 3 vectors which represents 3 vertices of a triangle
 	/*vertexBufferData2 := [9]gl.Float{	// N.B. We can't use []gl.Float, as that is a slice
@@ -126,6 +123,17 @@ func main() {
 	// Load the texture
 	texture := loadTGA("liske.tga")
 	var textureID gl.Int = gl.GetUniformLocation(programID, gl.GLString("myTextureSampler"))
+
+	// "Bind" the newly created texture
+	gl.BindTexture(gl.TEXTURE_2D, (gl.Uint)(textureID))
+
+	// Give the image to OpenGL
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB,
+		gl.Sizei(bitmap.width), gl.Sizei(bitmap.height),
+		0, gl.BGR, gl.UNSIGNED_BYTE, gl.Pointer(&bitmap.data[0]))
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.GenerateMipmap(gl.TEXTURE_2D)
 
 	// Three consecutive floats give a single 3D vertex
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2 = 12 triangles,
@@ -183,78 +191,43 @@ func main() {
 	// Two UV coordinated for each vertex.  They were created with
 	// Blender.  You'll learn shortly how to do this yourself.
 	uvBufferData := [...]gl.Float{
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-		0.0, 1.0,
-				// 0.000059, 1.0-0.000004, 
-				//       0.000103, 1.0-0.336048, 
-			   //       0.335973, 1.0-0.335903, 
-			  //       1.000023, 1.0-0.000013, 
-			  //       0.667979, 1.0-0.335851, 
-			  //       0.999958, 1.0-0.336064, 
-			  //       0.667979, 1.0-0.335851, 
-			  //       0.336024, 1.0-0.671877, 
-			  //       0.667969, 1.0-0.671889, 
-			  //       1.000023, 1.0-0.000013, 
-			  //       0.668104, 1.0-0.000013, 
-			  //       0.667979, 1.0-0.335851, 
-			  //       0.000059, 1.0-0.000004, 
-			  //       0.335973, 1.0-0.335903, 
-			  //       0.336098, 1.0-0.000071, 
-			  //       0.667979, 1.0-0.335851, 
-			  //       0.335973, 1.0-0.335903, 
-			  //       0.336024, 1.0-0.671877, 
-			  //       1.000004, 1.0-0.671847, 
-			  //       0.999958, 1.0-0.336064, 
-			  //       0.667979, 1.0-0.335851, 
-			  //       0.668104, 1.0-0.000013, 
-			  //       0.335973, 1.0-0.335903, 
-			  //       0.667979, 1.0-0.335851, 
-			  //       0.335973, 1.0-0.335903, 
-			  //       0.668104, 1.0-0.000013, 
-			  //       0.336098, 1.0-0.000071, 
-			  //       0.000103, 1.0-0.336048, 
-			  //       0.000004, 1.0-0.671870, 
-			  //       0.336024, 1.0-0.671877, 
-			  //       0.000103, 1.0-0.336048, 
-			  //       0.336024, 1.0-0.671877, 
-			  //       0.335973, 1.0-0.335903, 
-			  //       0.667969, 1.0-0.671889, 
-			  //       1.000004, 1.0-0.671847, 
-			  //       0.667979, 1.0-0.335851,
+
+		0.000059, 1.0 - 0.000004,
+		0.000103, 1.0 - 0.336048,
+		0.335973, 1.0 - 0.335903,
+		1.000023, 1.0 - 0.000013,
+		0.667979, 1.0 - 0.335851,
+		0.999958, 1.0 - 0.336064,
+		0.667979, 1.0 - 0.335851,
+		0.336024, 1.0 - 0.671877,
+		0.667969, 1.0 - 0.671889,
+		1.000023, 1.0 - 0.000013,
+		0.668104, 1.0 - 0.000013,
+		0.667979, 1.0 - 0.335851,
+		0.000059, 1.0 - 0.000004,
+		0.335973, 1.0 - 0.335903,
+		0.336098, 1.0 - 0.000071,
+		0.667979, 1.0 - 0.335851,
+		0.335973, 1.0 - 0.335903,
+		0.336024, 1.0 - 0.671877,
+		1.000004, 1.0 - 0.671847,
+		0.999958, 1.0 - 0.336064,
+		0.667979, 1.0 - 0.335851,
+		0.668104, 1.0 - 0.000013,
+		0.335973, 1.0 - 0.335903,
+		0.667979, 1.0 - 0.335851,
+		0.335973, 1.0 - 0.335903,
+		0.668104, 1.0 - 0.000013,
+		0.336098, 1.0 - 0.000071,
+		0.000103, 1.0 - 0.336048,
+		0.000004, 1.0 - 0.671870,
+		0.336024, 1.0 - 0.671877,
+		0.000103, 1.0 - 0.336048,
+		0.336024, 1.0 - 0.671877,
+		0.335973, 1.0 - 0.335903,
+		0.667969, 1.0 - 0.671889,
+		1.000004, 1.0 - 0.671847,
+		0.667979, 1.0 - 0.335851,
 	}
 
 	// Time to draw this sucker.
@@ -282,7 +255,6 @@ func main() {
 		gl.Pointer(&uvBufferData),
 		gl.STATIC_DRAW)
 
-
 	// Enable Z-buffer
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
@@ -307,9 +279,6 @@ func main() {
 		gl.BindTexture(gl.TEXTURE_2D, texture)
 		gl.Uniform1i(textureID, 0)
 
-
-
-
 		// 1st attribute buffer: vertices
 		gl.EnableVertexAttribArray(0)
 		gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
@@ -325,14 +294,12 @@ func main() {
 		gl.EnableVertexAttribArray(1)
 		gl.BindBuffer(gl.ARRAY_BUFFER, uvBuffer)
 		gl.VertexAttribPointer(
-			1,		// Attribute 1.  Again, no particular reason, but must match layout
-			2,		// size
+			1,        // Attribute 1.  Again, no particular reason, but must match layout
+			2,        // size
 			gl.FLOAT, // Type
-			gl.FALSE,	// normalized?
+			gl.FALSE, // normalized?
 			0,
-			nil)	// array buffer offset
-
-
+			nil) // array buffer offset
 
 		// Draw the cube!
 		gl.DrawArrays(gl.TRIANGLES, 0, 12*3) // Starting from vertex 0, 3 vertices total -> triangle
