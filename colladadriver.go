ed4f2863f917ff2591697f2e59e0aa34aa4711b7
ysgard <ysgard@gmail.com>
2013-04-15 08:11:51 -0700

    Initial attempts at a COLLADA importer, failing so far

---
+++ colladadriver.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/xml"
+	//"errors"
+	"fmt"
+	"os"
+)
+
+func BuildModel(filename string) *Collada {
+	file, err := os.Open(filename)
+	fi, err := file.Stat()
+	filelen := fi.Size()
+	buf := make([]byte, filelen)
+	read, err := file.Read(buf)
+	if read != int(filelen) {
+		fmt.Fprintf(os.Stderr, "Could not read complete contents of file: %d read vs %d size", read, filelen)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
+
+	c := new(Collada)
+	err = xml.Unmarshal(buf, c)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
+	return c
+}
+
+func main() {
+	c := BuildModel("world_tut/cone.dae")
+	c.Debug()
+}
